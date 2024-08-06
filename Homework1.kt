c80cc7ebfe001f938f8a308e612ab95e92a5e86f
Ahmet Ozan Yıldırım <147758703+ahmetozanyildirim@users.noreply.github.com>
2024-08-07 01:34:06 +0300

    Add files via upload

---
+++ Homework1.kt
@@ -0,0 +1,242 @@
+    package com.example.myapplication.variables
+
+fun main() {
+
+    //Ahmet Ozan Yıldırım
+
+    //Şehir
+    var city = "Çanakkale"
+    println("Şehir: $city")
+    println("--------------")
+
+    //Ülke
+    var country = "Türkiye"
+    println("Ülke: $country")
+    println("--------------")
+
+    //Telefon Numarası
+    var phoneNumber = "9050****4799"
+    println("Telefon numarası: +$phoneNumber")
+    println("--------------")
+
+    //Posta Kodu
+    var postCode = "34***"
+    println("Posta kodu: $postCode")
+    println("--------------")
+
+    //Email
+    var email = "ahozyildirim@gmail.com"
+    println("Email: $email")
+    println("--------------")
+
+    //Meslek
+    var occupancy = "Bilgisayar Mühendisi"
+    println("Meslek: $occupancy")
+    println("--------------")
+
+    //Stok Miktarı
+    var stock_amount = 200
+    println("Stok miktarı: $stock_amount adet")
+    println("--------------")
+
+    //Müşteri Adı
+    val customerName = "Ahmet Ozan"
+    println("Müşteri adı: $customerName")
+    println("--------------")
+
+    //Bakiye
+    var balance = 3500
+    println("Bakiye: $balance TL")
+    println("--------------")
+
+    //Doğum Günü
+    val birthDate= "8 Ocak"
+    println("Doğum günü: $birthDate")
+    println("--------------")
+
+    //Maaş
+    var wage = 35000
+    println("Maaş: $wage TL")
+    println("--------------")
+
+    //Medeni Durum
+    var marital_status = "Bekar"
+    println("Medeni durum: $marital_status")
+    println("--------------")
+
+    //Ürün Yorum
+    var productComment = "Harika bir ürün"
+    println("Ürün yorum: $productComment")
+    println("--------------")
+
+    //Ödeme Tarihi
+    var paymentDate = "27 Mayıs 2024"
+    println("Ödeme tarihi: $paymentDate")
+    println("--------------")
+
+    //Ödeme
+    var payment = 25000
+    println("Ödeme: $payment TL")
+    println("--------------")
+
+    //Sipariş Adeti
+    var totalOrder = 5
+    println("Sipariş adeti: $totalOrder adet")
+    println("--------------")
+
+    //Araba Modeli
+    val carModel = "Audi A8"
+    println("Araba modeli: $carModel")
+    println("--------------")
+
+    //Kitap Adı
+    val bookName = "Fahrenheit 451"
+    println("Kitap adı: $bookName")
+    println("--------------")
+
+    //Yayınlama Tarihi
+    val publishDate = "27 Eylül 2008"
+    println("Yayınlama tarihi: $publishDate")
+    println("--------------")
+
+    //İndirim Miktarı
+    var discount_amount = 25
+    println("İndirim miktarı: %$discount_amount")
+    println("--------------")
+
+    //Oda Sayısı
+    val roomCount = 3
+    println("Oda sayısı: $roomCount adet")
+    println("--------------")
+
+    //Enlem
+    var latitude = 40.094548
+    println("Enlem: $latitude")
+    println("--------------")
+
+    //Boylam
+    var longitude = 26.224955
+    println("Boylam: $longitude")
+    println("--------------")
+
+    //Ürün Adı
+    var productName = "Monster Abra A5"
+    println("Ürün adı: $productName")
+    println("--------------")
+
+    //Yemek Fiyatı
+    var foodPrice = 135.99
+    println("Yemek fiyatı: $foodPrice TL")
+    println("--------------")
+
+    //Marka
+    val brand = "Apple"
+    println("Marka: $brand")
+    println("--------------")
+
+    //Müzik Adı
+    val musicName = "Sam Smith - Writing`s On The Wall"
+    println("Müzik adı: $musicName")
+    println("--------------")
+
+    //Video Süresi
+    var videoDuration = 3
+    println("Video süresi: $videoDuration dakika")
+    println("--------------")
+
+    //Ürün Puanı
+    var productRating = 4.8
+    println("Ürün puanı: 5 üzerinden $productRating puan")
+    println("--------------")
+
+    //Resim Adı
+    val paintingName = "La persistència de la memòria"
+    println("Resim adı: $paintingName")
+    println("--------------")
+
+    //Dosya Formatı
+    var fileFormat = "jpg"
+    println("Dosya formatı: .$fileFormat")
+    println("--------------")
+
+    //Renk
+    var color = "Kırmızı"
+    println("Renk: $color")
+    println("--------------")
+
+    //Renk Kodu
+    var colorCode = "#fdf6e3"
+    println("Renk kodu: $colorCode")
+    println("--------------")
+
+    //Telefon Modeli
+    val phoneModel = "Iphone 12"
+    println("Telefon modeli: $phoneModel")
+    println("--------------")
+
+    //Ekran Boyutu
+    val screenSize = 6.3
+    println("Ekran boyutu: $screenSize inç")
+    println("--------------")
+
+    //Ağırlık
+    val weight = 87.4
+    println("Ağırlık: $weight kg")
+    println("--------------")
+
+    //Ulusal Gün
+    val nationalDay = "29 Ekim"
+    println("Ulusal gün: $nationalDay")
+    println("--------------")
+
+    //Tatil Günü
+    val holiday = "Pazar"
+    println("Tatil günü: $holiday günü")
+    println("--------------")
+
+    //Rezervasyon Tarihi
+    var reservationDate = "22 Aralık 2024"
+    println("Rezervasyon tarihi: $reservationDate")
+    println("--------------")
+
+    //Sokak Adı
+    val streetName = "Gül Sokak"
+    println("Sokak adı: $streetName")
+    println("--------------")
+
+    //Otobüs Hattı
+    val busLine = "500T"
+    println("Otobüs hattı: $busLine")
+    println("--------------")
+
+    //Kalan Dakika
+    var remainingMinutes = 36
+    println("Kalan dakika: $remainingMinutes dakika")
+    println("--------------")
+
+    //Takip Kodu
+    val trackingCode = "33BPZ29F"
+    println("Takip kodu: $trackingCode")
+    println("--------------")
+
+    //Kupon Süresi
+    var couponDuration = 35
+    println("Kupon süresi: $couponDuration gün")
+    println("--------------")
+
+    //Kupon Kodu
+    var couponCode = "SEPETTE100"
+    println("Kupon kodu: $couponCode")
+    println("--------------")
+
+    //Fatura Adresi
+    var billingAddress = "Kestel, KYK ALANYA YURDU, ÜNİVERSİTE CAD NO:175, 07400 Alanya/Antalya"
+    println("Fatura adresi: $billingAddress")
+    println("--------------")
+
+
+
+
+
+
+}
